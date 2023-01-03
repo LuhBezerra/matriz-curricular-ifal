f3b001452b744f25ed44a2fdb999cfd70aa3ece4
lubezerra <lucilenebezerra.of@gmail.com>
2023-01-03 17:28:39 -0300

    feat: add info modal

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,5 @@
-import { Select } from "antd";
+import { Modal, Select } from "antd";
+import { InformationCircleIcon } from "@heroicons/react/24/solid";
 
 export const Header = ({ selectOptions, onSelectChange }) => {
   const onFilterOption = (input, option) =>
@@ -9,11 +10,51 @@ export const Header = ({ selectOptions, onSelectChange }) => {
       .toUpperCase()
       .localeCompare((optionB?.label ?? "").toUpperCase());
 
+  const info = () => {
+    Modal.info({
+      title: "Dicas rápidas",
+      content: (
+        <div>
+          <strong>Sobre</strong>
+          <p>
+            A aplicação foi idealizada no intuito de ajudar alunos
+            desperiodizados que tem dificuldade em saber quais disciplinas estão
+            restando para finalizar a carga horária total
+          </p>
+          <br />
+          <strong>Como usar</strong>
+          <p>- Cadastrar todas as disciplinas concluídas através do select</p>
+          <p>
+            - Na aba 'Carga horária restante' é possível verificar as
+            informações referentes aos eixos curriculares, tanto a carga horária
+            restante quanto a sugestão de disciplinas disponíveis para concluir
+            cada eixo
+          </p>
+          <br />
+          <strong>Informações extras</strong>
+          <p>
+            A aplicação é um PWA então é possível instalá-lo no seu dispositivo
+            e salvar os dados localmente
+          </p>
+        </div>
+      ),
+      closable: true,
+      centered: true,
+      maskClosable: true
+    });
+  };
+
   return (
     <header className="w-full p-4 bg-green-600 h-28 flex flex-col items-center justify-between">
       <h1 className="lg:mt-4 text-xl text-center lg:text-3xl font-bold text-white">
         Carga horária - Sistemas de Informação / IFAL Maceió
       </h1>
+      <button
+        className="absolute bg-white h-12 w-12 right-4 lg:right-10 bottom-10 rounded-full"
+        onClick={info}
+      >
+        <InformationCircleIcon className="text-green-500 hover:text-green-600"/>
+      </button>
       <Select
         autoClearSearchValue
         size="large"
